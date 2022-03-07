1081c5149423a4210f305c42805f55227eb45ed7
Jürgen Efeish <jefeish@github.com>
2022-03-07 04:11:46 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# issue-patrol
-observe and inspect issues
+# Issue-Patrol
+
+Observe and inspect Repository issues.
+
+This App visualizes Issues, their age and activity, based on last update.
+The goal is to get a quick overview on the status of Repo Issues.
+
+![overview](docs/images/issue_patrol.png)
+
+## Run the App
+
+1. Create an `.env` file
+   1. Copy the `.env.sample` file to `.env`, provide a GitHub PAT for the `TOKEN`
+
+   Sample `.env` content
+
+   ```bash
+   API_URL=api.github.com
+   PORT=8008
+   TOKEN="ghp_..."
+   ```
+
+2. Start the App
+
+    ```bash
+    npm start
+    ```
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const hbs = require('handlebars')
 const path = __dirname + '/'
 require('dotenv').config();
 const util = require('util')
+const moment = require('moment');
 
 // --------------------------------------------------
 const PORT = process.env.PORT
@@ -17,11 +18,12 @@ const octokit = new Octokit({ auth: TOKEN });
 // --------------------------------------------------
 // 
 // --------------------------------------------------
-async function getIssues(owner, repo) {
+async function getIssues(owner, repo, state, page_limit) {
     const { data } = await octokit.rest.issues.listForRepo({
         owner: owner,
         repo: repo,
-        per_page: 2,
+        state: state,
+        per_page: page_limit,
     });
     
     return data;
@@ -31,25 +33,73 @@ async function getIssues(owner, repo) {
 // 
 // --------------------------------------------------
 app.get('/', function (req, res) {
+    const state = req.query.issue_state || 'open'
+    const repo = req.query.repo || 'test'
+    const sortTasks = req.query.sortTasks 
+    const page_limit = req.query.page_limit || 100
+    
+    getIssues('githubcustomers', repo, state, page_limit).then(result => {
+        const TODAY = moment(new Date())
+        const r = result
 
-    getIssues('jefeish', 'test').then(result => {
-        console.log("title:" + util.inspect(result[0].title))
-        console.log("state:" + util.inspect(result[0].state))
-        console.log("created_at:" + util.inspect(result[0].created_at))
-        console.log("updated_at:" + util.inspect(result[0].updated_at))
-        console.log("html_url:" + util.inspect(result[0].html_url))
-        
         fs.readFile(path + 'templates/issues-gantt.hbs', 'utf8', (err, tpl) => {
             if (err) {
                 console.error(err)
                 return
             }
 
-            var header = fs.readFileSync(path + 'templates/header.html', 'utf8');
+            let issueGrid = []
+
+            for (let i = 0; i < r.length; i++) {
+
+                if (!r[i].pull_request) {
+                    let row = {}
+                    row['id'] = r[i].number
+                    row['title'] = r[i].title
+                    row['state'] = r[i].state
+                    const start = moment(r[i].created_at)
+                    const end = moment(r[i].updated_at)
+                    const active = end.diff(start, 'days')
+                    row['start'] = start
+                    const state = r[i].state
+
+                    if ( state !== 'open' ) {
+                        row['end'] = end
+                        row['done'] = 100
+                    }
+                    else {
+                        const time_inprogress = end - start
+                        const days_inprogress = Math.ceil(time_inprogress / (1000 * 60 * 60 * 24))
+                        const days_since_last_update = Math.ceil(TODAY.diff(end, 'hours') / 24)
+                        const days_total = days_since_last_update + days_inprogress
+                        const active = Math.floor(days_total * days_inprogress * 0.1)
+                        row['end'] = TODAY
+                        row['done'] = active
+                    }
+ 
+                    issueGrid.push(row)
+                }
+            }
+                
+            if (issueGrid.length == 0) {
+                issueGrid.push({ id:null, title:'No Issues found', state:null, start:null, end:null, a:null, done:null, b:null})
+            }
+
             var template = hbs.compile(tpl);
             var data = {
-                "header": header
-            };
+                "sortTasks": sortTasks === 'on' ? 'true' : 'false',
+                "repo": repo,
+                "state": state,
+                "page_limit": page_limit,
+                "checked_all": state === 'all' ? 'checked' : '',
+                "checked_open": state === 'open' ? 'checked' : '',
+                "checked_closed": state === 'closed' ? 'checked' : '',
+                "checked_reverse": sortTasks === 'on' ? 'checked' : '',
+                "issue_filter": " - [ State: "+ state +" ]",
+                "height": 30 * r.length + 100,
+                "issues": issueGrid
+            };              
+        
             var result = template(data);
             res.send(result)
         });
